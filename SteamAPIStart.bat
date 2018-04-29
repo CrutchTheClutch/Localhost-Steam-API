9ac5fb0ab124a5dcdce39330d4faab55b039aa4f
William Crutchfield <william.r.crutchfield@gmail.com>
2018-04-29 16:28:01 -0700

    Initial Commit

---
+++ SteamAPIStart.bat
@@ -0,0 +1,11 @@
+@echo off
+cls
+ECHO //////////////////////////////////////////////////
+ECHO // STEAM API WEB SERVER
+ECHO //////////////////////////////////////////////////
+ECHO.
+ECHO Visit "http://localhost:4000/static/index.html" to begin
+ECHO.
+ECHO Press Control+C (^C) to STOP
+ECHO.
+node server.js
---
+++ server.js
@@ -0,0 +1,46 @@
+const steamKey = "";	// Place your Steam API key in this variable as a string
+const steamID = "";		// Place your Steam ID in this variable as a string
+
+var express = require('express');
+var request = require('request');
+var app = express();
+app.set('view engine', 'ejs');
+
+
+// Homepage
+// (localhost:4000/)
+app.get('/', function(req, res) {
+    res.render('pages/index');
+});
+
+// Achievement Tracker
+// (localhost:4000/:appid) Replace ":appid" with id of game you would like to track
+app.get('/:appid', function(req, res) {
+    let url = 'https://api.steampowered.com/ISteamUserStats/GetPlayerAchievements/v0001/?appid=' + req.params.appid + '&key=' + steamKey + '&steamid=' + steamID;
+    let aTotal;
+    let aEarned = 0;
+    let aPercent;
+
+
+    request.get(url, function(error, steamHttpResponse, steamHttpBody) {
+        let data = JSON.parse(steamHttpBody);
+        let gameName = data.playerstats.gameName;
+        aTotal = data.playerstats.achievements.length;
+
+        for ( var i = 0; i < aTotal; i++) {
+            if (data.playerstats.achievements[i].achieved === 1) {
+                aEarned++;
+            }
+        }
+
+        aPercent = ((aEarned / aTotal) * 100).toFixed(2);
+        res.render('pages/achieve', {aEarned: aEarned, aTotal: aTotal, aPercent: aPercent});
+        console.log("Game: " + gameName);
+    });
+});
+
+app.use('/static', express.static('public'));
+
+var port = 4000;
+var server = app.listen(port);
+console.log('Listening on port ' + port);
