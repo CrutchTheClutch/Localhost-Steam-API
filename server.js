dcd461e2afd519306abce2ea57aa95074e38d1f4
William Crutchfield <william.r.crutchfield@gmail.com>
2019-03-17 13:58:04 -0700

    Fixed Achievement.length error
    In some cases, using the length method on the achievements variable resulted in an error.  Changed the way achievements were counted to remove this issue.

--- server.js
+++ server.js
@@ -17,7 +17,7 @@ app.get('/', function(req, res) {
 // (localhost:4000/:appid) Replace ":appid" with id of game you would like to track
 app.get('/:appid', function(req, res) {
     let url = 'https://api.steampowered.com/ISteamUserStats/GetPlayerAchievements/v0001/?appid=' + req.params.appid + '&key=' + steamKey + '&steamid=' + steamID;
-    let aTotal;
+    let aTotal = 0;
     let aEarned = 0;
     let aPercent;
 
@@ -25,17 +25,22 @@ app.get('/:appid', function(req, res) {
     request.get(url, function(error, steamHttpResponse, steamHttpBody) {
         let data = JSON.parse(steamHttpBody);
         let gameName = data.playerstats.gameName;
-        aTotal = data.playerstats.achievements.length;
+        let achievements = data.playerstats.achievements;
+        
+        for (var achievement in achievements) {
+            if (achievements.hasOwnProperty(achievement)) {
+                aTotal++;
+            }
+        }
 
         for ( var i = 0; i < aTotal; i++) {
-            if (data.playerstats.achievements[i].achieved === 1) {
+            if (achievements[i].achieved === 1) {
                 aEarned++;
             }
         }
 
         aPercent = ((aEarned / aTotal) * 100).toFixed(2);
         res.render('pages/achieve', {aEarned: aEarned, aTotal: aTotal, aPercent: aPercent});
-        console.log("Game: " + gameName);
     });
 });
 
